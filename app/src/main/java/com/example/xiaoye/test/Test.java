bbe0b346ef889b52d007b0c44f92039096c97900
lyws1986 <773150200@qq.com>
2019-05-15 15:38:45 +0800

    commit02

--- app/src/main/java/com/example/xiaoye/test/Test.java
+++ app/src/main/java/com/example/xiaoye/test/Test.java
@@ -1,8 +1,15 @@
 package com.example.xiaoye.test;
 
+import android.util.Log;
+
 /**
  * Created by xiaoye on 2019/5/15.
+ * 同步到github查看。
  */
-
 public class Test {
+
+
+    public void Talk(){
+        Log.i("1","i will sent to github");
+    }
 }
